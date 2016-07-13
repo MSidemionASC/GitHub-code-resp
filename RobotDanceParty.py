d9a1a3d6d135cdc8ea08cf6524f61d14c54d9c5f
MSidemionASC <michels2394@gmail.com>
2016-07-13 12:40:19 -0400

    Making the robot dance
    Using multiple functions like forward, backward and motors.

---
+++ RobotDanceParty.py
@@ -0,0 +1,50 @@
+from Myro import *
+
+def danceMoveOne():
+    backward(1, .5)
+    forward(1, .5)
+    backward(1, .5)
+    forward(1, .5)
+def danceMoveTwo():
+    motors(-1, 1, .4)
+    backward(1, .5)
+    forward(1, .5)
+    backward(1, .5)
+    forward(1, .5)
+    backward(1, 1)
+def danceMoveThree():
+    motors(1, -1, .8)
+    forward(1, 1)
+    backward(1, .5)
+    forward(1, .5)
+    backward(1, .5)
+    forward(1, .5)
+def danceMoveFour():
+    backward(1, 1)
+    motors(-1, 1, .4)
+    backward(1, .5)
+    forward(1, .5)
+    motors(-1, 1, 7)
+    motors(1, -1, 7)
+def danceMoveFive():
+    backward(1, .5)
+    forward(1, .5)
+    motors(-1, 1, 7)
+    motors(1, -1, 7)
+def danceMoveSix():
+    backward(1, .3)
+def danceMoveSeven():
+    forward(1, 3)
+danceMoveSix()
+danceMoveSeven()
+danceMoveOne()
+danceMoveOne()
+danceMoveOne()
+danceMoveTwo()
+danceMoveThree()
+danceMoveFour()
+danceMoveFive()
+danceMoveFive()
+
+
+   
