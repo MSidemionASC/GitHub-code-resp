27775cce3d69367b30cf2bab968c2fe29ad35843
MSidemionASC <michels2394@gmail.com>
2016-07-15 10:05:56 -0400

    Finding the blob
    takes pictures uses loops, and if statements to scan picture and take it
    and move towards blob.

--- RobotDanceParty.py
+++ RobotDanceParty.py
@@ -1,40 +1,40 @@
 from Myro import *
 
 def danceMoveOne():
-    backward(1, .5)
-    forward(1, .5)
-    backward(1, .5)
-    forward(1, .5)
+    backward(1, .5)
+    forward(1, .5)
+    backward(1, .5)
+    forward(1, .5)
 def danceMoveTwo():
-    motors(-1, 1, .4)
-    backward(1, .5)
-    forward(1, .5)
-    backward(1, .5)
-    forward(1, .5)
-    backward(1, 1)
+    motors(-1, 1, .4)
+    backward(1, .5)
+    forward(1, .5)
+    backward(1, .5)
+    forward(1, .5)
+    backward(1, 1)
 def danceMoveThree():
-    motors(1, -1, .8)
-    forward(1, 1)
-    backward(1, .5)
-    forward(1, .5)
-    backward(1, .5)
-    forward(1, .5)
+    motors(1, -1, .8)
+    forward(1, 1)
+    backward(1, .5)
+    forward(1, .5)
+    backward(1, .5)
+    forward(1, .5)
 def danceMoveFour():
-    backward(1, 1)
-    motors(-1, 1, .4)
-    backward(1, .5)
-    forward(1, .5)
-    motors(-1, 1, 7)
-    motors(1, -1, 7)
+    backward(1, 1)
+    motors(-1, 1, .4)
+    backward(1, .5)
+    forward(1, .5)
+    motors(-1, 1, 7)
+    motors(1, -1, 7)
 def danceMoveFive():
-    backward(1, .5)
-    forward(1, .5)
-    motors(-1, 1, 7)
-    motors(1, -1, 7)
+    backward(1, .5)
+    forward(1, .5)
+    motors(-1, 1, 7)
+    motors(1, -1, 7)
 def danceMoveSix():
-    backward(1, .3)
+    backward(1, .3)
 def danceMoveSeven():
-    forward(1, 3)
+    forward(1, 3)
 danceMoveSix()
 danceMoveSeven()
 danceMoveOne()
@@ -47,4 +47,5 @@ danceMoveFive()
 danceMoveFive()
 
 
-   
+   
+\ No newl
